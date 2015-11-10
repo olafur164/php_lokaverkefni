9fdb961ff073f60384d11f023536f9054b835e85
olafur164 <olafurholm17@gmail.com>
2015-11-10 11:38:01 +0000

    First Setup

---
+++ app/TimeEntries.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TimeEntries extends Model
+{
+    //
+    protected $table = 'time_entries';
+}
---
+++ database/migrations/2015_11_10_105601_create_time_entry.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeEntry extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->date('date');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('time_entries');
+    }
+}
